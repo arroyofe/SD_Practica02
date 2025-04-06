af550b25a42de394df7eed3e08d8060d0386ec7d
Fernando <far0006@alu.ubu.es>
2025-04-06 12:07:34 +0200

    Initial commit

---
+++ src/main/java/practica02/SdPractica02Application.java
@@ -0,0 +1,13 @@
+package practica02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdPractica02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdPractica02Application.class, args);
+	}
+
+}
