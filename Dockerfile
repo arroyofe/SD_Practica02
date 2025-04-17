b7847254f36932c47198f29423519441dc610c5e
Fernando <far0006@alu.ubu.es>
2025-04-17 10:48:20 +0200

    Version 6
    Modificaciones que permiten levantar la aplicación y la base de datos desde Docker-compose

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# Construcción del JAR con MAVEN
+FROM jelastic/maven:3.9.9-openjdk-17.0.14-almalinux-9
+LABEL authors="Fenando Arroyo"
+WORKDIR /SD_Practica02
+COPY . .
+#  RUN mvn clean install
+
+CMD mvn spring-boot:run
--- src/main/java/practica02/repository/UserRepository.java
+++ src/main/java/practica02/repository/UserRepository.java
@@ -1,17 +1,22 @@
 package practica02.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
 import practica02.model.User;
 
 import java.util.List;
-
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> id(long id);
 
+    boolean existsById(Long id);
+
+    Optional<User> findById(Long id);
+
+    void save(User user);
 }
